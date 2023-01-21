e455da5f823f4adb4a89683f51980f40b8442279
AdeFuaad <adesholafuaad@gmail.com>
2023-01-21 15:25:52 -0500

    updated javascript codes

--- script.js
+++ script.js
@@ -8,16 +8,12 @@ const numberChar = "0123456789";
 const specialChar = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
 
 function lengthError() {
-
   alert("Password length must be more than 8 but less than 128");
-
 }
 
 function charError() {
-
   alert("You must select atleast one of the characters to generate a password");
   return "Select at least one of the criterias";
-  
 }
 
 function verifyInput() {
